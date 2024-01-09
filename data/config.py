24da0fc5f3edaa012c70437564c34e54577a2e4e
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-01-09 23:00:31 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,7 +1,11 @@
 API_ID: int = 6
 API_HASH: str = 'eb06d4abfb49dc3eeb1aeb98ae0f581e'
-DELAY_BETWEEN_CLICK_RANGE: list[int] = [10, 15]
 MIN_CLICKS_COUNT: int = 1
-AUTO_BUY_ITEMS: bool = False
+AUTO_BUY_ENERGY_BOOST: bool = False
+AUTO_BUY_SPEED_BOOST: bool = True
+AUTO_BUY_CLICK_BOOST: bool = True
+USE_PROXY_FROM_FILE: bool = True
+SLEEP_BETWEEN_CLICK: list[int] = [10, 15]
 SLEEP_BEFORE_BUY_MERGE: list[int] = [10, 15]
-USE_PROXY_FROM_FILE: bool = False
+SLEEP_BEFORE_ACTIVATE_FREE_BUFFS: list[int] = [10, 15]
+SLEEP_BEFORE_ACTIVATE_TURBO: list[int] = [10, 15]
