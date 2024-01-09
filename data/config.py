fe3df49110ff449b591e915a8c229495a6533653
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-01-09 17:13:58 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,3 +4,4 @@ DELAY_BETWEEN_CLICK_RANGE: list[int] = [10, 15]
 MIN_CLICKS_COUNT: int = 1
 AUTO_BUY_ITEMS: bool = False
 SLEEP_BEFORE_BUY_MERGE: list[int] = [10, 15]
+USE_PROXY_FROM_FILE: bool = False
